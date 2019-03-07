d3595df2f9c4ea7b90d4f1f4fe39e1862ee1c0b6
helloprash <helloprash7@gmail.com>
2019-03-07 13:15:18 +0530

    Full

--- setup.py
+++ setup.py
@@ -14,18 +14,25 @@ syspath = r"D:\Users\PB6\AppData\Local\Programs\Python\Python37\DLLs"
 
 includes      = []
 include_files = [syspath + '/tcl86t.dll', syspath + '/tk86t.dll', 'Jnj48.ico', 'chromedriver.exe', 'phantomjs.exe', 'killPhantom.bat', 'killChrome.bat']
+excludes = [""]
 
+build_exe_options = {"excludes": excludes,
+                                  "includes": includes, 
+                                  "include_files": include_files,
+                                  "packages":["tkinter","selenium"],
+                     "optimize": 2}
 '''
 if sys.platform == 'win32':
     base = "Win32GUI"
 '''
 
-executables = [cx_Freeze.Executable("UI.py", base=base, icon="Jnj48.ico")]
+executables = [cx_Freeze.Executable("CATSWeb Automation Tool.py", base=base, icon="Jnj48.ico")]
 
 cx_Freeze.setup(
-    name = "CatsWeb Complaint Handler",
-    options = {"build_exe": {"includes": includes, "include_files": include_files,"packages":["tkinter","selenium"]}},
-    version = "1.0.0",
-    description = "CatsWeb complaint handler for Level 1 and Level 2 complaints",
+    name = "CATSWeb Automation Tool",
+    options = {"build_exe": build_exe_options},
+    version = "1.0",
+    description = """CatsWeb complaint handler for Level 1 and Level 2 complaints
+    Copyright +\u00a9+ 2019 Tata Consultancy Services Limited""",
     executables = executables
     )
