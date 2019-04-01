e8bdb5d7e87da468f7e45b539b8dd7ba7e27fc2c
Prashanth Kumar <31364644+helloprash@users.noreply.github.com>
2019-04-01 10:56:59 +0530

    Updated Setup

--- setup.py
+++ setup.py
@@ -6,14 +6,16 @@ import selenium
 
 base = None
 
-os.environ['TCL_LIBRARY'] = r'D:\Users\PB6\AppData\Local\Programs\Python\Python37\tcl\tcl8.6'
-os.environ['TK_LIBRARY'] = r'D:\Users\PB6\AppData\Local\Programs\Python\Python37\tcl\tk8.6'
+#syspath = r"D:\Users\PB6\AppData\Local\Programs\Python\Python37\DLLs"
 
-
-syspath = r"D:\Users\PB6\AppData\Local\Programs\Python\Python37\DLLs"
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
 
 includes      = []
-include_files = [syspath + '/tcl86t.dll', syspath + '/tk86t.dll', 'Jnj48.ico', 'chromedriver.exe', 'phantomjs.exe', 'killPhantom.bat', 'killChrome.bat']
+include_files = [os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'), 
+                    os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),'Jnj48.ico', 'chromedriver.exe', 'phantomjs.exe', 'killPhantom.bat', 'killChrome.bat']
+#include_files = [syspath + '/tcl86t.dll', syspath + '/tk86t.dll', 'Jnj48.ico', 'chromedriver.exe', 'phantomjs.exe', 'killPhantom.bat', 'killChrome.bat']
 excludes = [""]
 
 build_exe_options = {"excludes": excludes,
@@ -21,10 +23,10 @@ build_exe_options = {"excludes": excludes,
                                   "include_files": include_files,
                                   "packages":["tkinter","selenium"],
                      "optimize": 2}
-'''
+
 if sys.platform == 'win32':
     base = "Win32GUI"
-'''
+
 
 executables = [cx_Freeze.Executable("CATSWeb Automation Tool.py", base=base, icon="Jnj48.ico")]
 
